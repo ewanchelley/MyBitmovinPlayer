c02d5b48c54ed36c101af2ba2c05c63155890c5f
chelle02 <ewan.d.chelley@bbc.co.uk>
2024-12-18 11:50:14 +0000

    Initial Commit

---
+++ MyBitmovinPlayer/MyBitmovinPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBitmovinPlayerApp.swift
+//  MyBitmovinPlayer
+//
+//  Created by Ewan Chelley on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBitmovinPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBitmovinPlayerTests/MyBitmovinPlayerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyBitmovinPlayerTests.swift
+//  MyBitmovinPlayerTests
+//
+//  Created by Ewan Chelley on 18/12/2024.
+//
+
+import Testing
+
+struct MyBitmovinPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
